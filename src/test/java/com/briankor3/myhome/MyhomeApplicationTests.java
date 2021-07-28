08004d28a47a3d958acc8018b0368893a88a3d01
GodCoder <mygodcoder@gmail.com>
2021-07-28 22:43:41 +0900

    init

---
+++ src/test/java/com/briankor3/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briankor3.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
